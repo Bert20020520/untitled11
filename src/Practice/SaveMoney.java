65e20930c3a51911574cb04fbf66e39f86bd50e6
Bert <1464668958q@qq.com>
2021-04-26 16:57:34 +0800

    Initial commit

---
+++ src/Practice/SaveMoney.java
@@ -0,0 +1,15 @@
+package Practice;
+
+import Practice.CommericalBank;
+
+public class SaveMoney {
+    public static void main(String args[]) {
+        int amount=8000;
+        CommericalBank bank= new CommericalBank();
+        bank.savedMoney = amount;
+        bank.year = 8.236;
+        bank.setInterestRate (0.035) ;
+        double interest2=bank.computerInterest() ;
+
+    }
+}
